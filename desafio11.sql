d081911597b33522984fc0569f07561dc0c3ce7d
Jamile <milli.santana2017@gmail.com>
2022-07-18 19:56:35 -0300

    adiciona requisito 11: Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,3 @@
-SELECT notes FROM `northwind-dev`.purchase_orders
+SELECT notes FROM northwind.purchase_orders
 where notes >= 'Purchase generated based on Order #30'
 AND notes <= 'Purchase generated based on Order #39';
